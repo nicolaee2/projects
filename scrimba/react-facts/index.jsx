1812f69e7a138908a0addaa2bd870dd018453f0d
nmartinescu <nmartinescu@bitdefender.com>
2024-12-23 21:26:14 +0200

    react facts: deleted comments

--- scrimba/react-facts/index.jsx
+++ scrimba/react-facts/index.jsx
@@ -3,13 +3,4 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
-/**
-Challenge: Project setup
-
-- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
-  Put the `<links />` to those fonts ABOVE the style.css link in index.html.
-  You may need to do some extra research to figure out how this 
-  works if you haven't done it before.
-*/
-
 root.render(<App />);
