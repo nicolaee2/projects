46e5499255ca350629d504ce78034dc5e021e9c3
nmartinescu <nmartinescu@bitdefender.com>
2024-12-23 21:25:20 +0200

    react-facts: finished project

--- scrimba/react-facts/components/Navbar.jsx
+++ scrimba/react-facts/components/Navbar.jsx
@@ -1,3 +1,10 @@
 export default function Navbar() {
-    return <h1>Navbar goes here</h1>;
+    return (
+        <header>
+            <nav>
+                <img src="../images/react-logo.png" alt="react logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    );
 }
